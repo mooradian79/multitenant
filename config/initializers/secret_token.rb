6ee0e0ed5784e684243e4260cdb3b0d6af55cd97
Martin Mooradian <martin_mooradian@yahoo.com>
2011-12-09 13:58:07 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Multitenant::Application.config.secret_token = '0a22747bc682271fee63484e1fb72fe3112161679fc93c8fbf37b947bfa6d2e2512b06b5b7b7a9e660016d59da4d4eab0438a8eecee3a3269bd77429beabdfae'
