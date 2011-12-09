822dfc1bdbb15b22dcd4fec654dac068387bddb7
Martin Mooradian <martin_mooradian@yahoo.com>
2011-12-09 16:22:35 -0500

    Added all the devise directories

--- Gemfile
+++ Gemfile
@@ -1,11 +1,13 @@
 source 'http://rubygems.org'
 
 gem 'rails', '3.0.7'
-
+gem 'devise', '1.2.rc'
+gem 'friendly_id', '3.2.1'
+gem 'heroku', :group => :development
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
-gem 'sqlite3'
+
 
 # Use unicorn as the web server
 # gem 'unicorn'
@@ -20,7 +22,7 @@ gem 'sqlite3'
 # Bundle the extra gems:
 # gem 'bj'
 # gem 'nokogiri'
-# gem 'sqlite3-ruby', :require => 'sqlite3'
+  gem 'sqlite3-ruby', :require => 'sqlite3'
 # gem 'aws-s3', :require => 'aws/s3'
 
 # Bundle gems for the local environment. Make sure to
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+puts 'SETTING UP EXAMPLE USERS'
+user1 = User.create! :name => 'First User', :email => 'user@test.com', :password => 'please', :password_confirmation => 'please'
+puts 'New user created: ' << user1.name
+user2 = User.create! :name => 'Other User', :email => 'otheruser@test.com', :password => 'please', :password_confirmation => 'please'
+puts 'New user created: ' << user2.name
+puts 'SETTING UP EXAMPLE SUBDOMAINS'
+subdomain1 = Subdomain.create! :name => 'foo'
+puts 'Created subdomain: ' << subdomain1.name
+subdomain2 = Subdomain.create! :name => 'bar'
+puts 'Created subdomain: ' << subdomain2.name
+user1.subdomains << subdomain1
+user1.save
+user2.subdomains << subdomain2
+user2.save
